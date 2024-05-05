36cacc9e6f621e8cf66ecf5a8976322522f7e29a
Jason <jasonetienne1@gmail.com>
2024-05-05 15:29:41 +0100

    fixing server connection

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,19 +1,21 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = process.env.MONGODB_URI || "";
-const client = new MongoClient(uri, {serverApi: {
+const URI = process.env.MONGODB_URI || "";
+const client = new MongoClient(URI, {
+  serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true
-}})
+    deprecationErrors: true,
+  },
+});
 
 try {
-    //Connect the Client to the Server
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  //Connect the Client to the Server
+  await client.connect();
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
 let db = client.db("employees");
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import records from "./routes/record.js"
 
 const PORT = process.env.PORT || 5050;
-const app = express = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
