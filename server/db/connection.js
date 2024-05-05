c295cf846774cd1c8d001d120c5462edf56ab7f7
Jason <jasonetienne1@gmail.com>
2024-05-05 14:29:41 +0100

    set up connection to Mongo

---
+++ server/db/connection.js
@@ -0,0 +1,21 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGODB_URI || "";
+const client = new MongoClient(uri, {serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+}})
+
+try {
+    //Connect the Client to the Server
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (error) {
+    console.log(error);
+}
+
+let db = client.db("employees");
+
+export default db;
